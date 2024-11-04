df189b2717f59357020418df19aa8d663663dd87
Shoaib Sarwar <devshoaibsarwar65@gmail.com>
2024-11-04 20:56:42 +0500

    improvement:: add new grid and move detail grid data to separate table

--- src/app/components/repositories/repositories.component.ts
+++ src/app/components/repositories/repositories.component.ts
@@ -1,12 +1,23 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { GithubService } from 'src/app//services/github.service';
 import { MatTableDataSource } from '@angular/material/table';
-import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatPaginator } from '@angular/material/paginator';
 import { RepositoryDetails } from 'src/app/interfaces/Repository';
-import { Subject, takeUntil } from 'rxjs';
+import { Subject } from 'rxjs';
 import 'ag-grid-enterprise';
 import { GridReadyEvent, IServerSideDatasource } from 'ag-grid-community';
 import { CheckboxComponent } from 'src/generic/checkbox/checkbox.component';
+
+const COMMON_GRID_OPTIONS = {
+  rowSelection: 'none',
+  paginationPageSizeSelector: [5, 10, 20, 50],
+  masterDetail: true,
+  pagination: true,
+  rowModelType: 'serverSide',
+  defaultColDef: {
+    flex: 1,
+  },
+};
 
 @Component({
   selector: 'app-repositories',
@@ -18,11 +29,14 @@ export class RepositoriesComponent implements OnInit {
   length = 0;
 
   gridOptions: any;
+  detailGridOptions: any;
   rowData: any;
+  detailRowData: any;
   page = 0;
   pageSize = 10;
   public context: any = null;
-  public overlayNoRowsTemplate: string = '<div style="padding: 10px; text-align: center;">No data available</div>'
+  public overlayNoRowsTemplate: string =
+    '<div style="padding: 10px; text-align: center;">No data available</div>';
 
   displayedColumns: string[] = ['name', 'description', 'slug', 'url'];
   dataSource: MatTableDataSource<RepositoryDetails[]>;
@@ -31,12 +45,24 @@ export class RepositoriesComponent implements OnInit {
 
   constructor(private githubService: GithubService) {
     this.dataSource = new MatTableDataSource();
+    this.detailGridOptions = {
+      columnDefs: [
+        { field: 'userId', headerName: 'User Id' },
+        { field: 'username', headerName: 'User' },
+        { field: 'repository.name', headerName: 'Repository' },
+        { field: 'totalCommits', headerName: 'Total Commits' },
+        { field: 'totalPRs', headerName: 'Total Pull Requests.' },
+        { field: 'totalIssues', headerName: 'Total Issues' },
+      ],
+      paginationPageSize: this.pageSize,
+      cacheBlockSize: this.pageSize,
+      ...COMMON_GRID_OPTIONS,
+    };
     this.gridOptions = {
       columnDefs: [
         {
           field: '_id',
           headerName: 'ID',
-          cellRenderer: 'agGroupCellRenderer',
         },
         { field: 'name', headerName: 'Name' },
         {
@@ -56,51 +82,15 @@ export class RepositoriesComponent implements OnInit {
           cellRenderer: CheckboxComponent,
         },
       ],
-      rowSelection: 'none',
-      paginationPageSize: this.pageSize,
-      paginationPageSizeSelector: [5, 10, 20, 50],
-      masterDetail: true,
       cacheBlockSize: this.pageSize,
-      pagination: true,
-      rowModelType: 'serverSide',
-      defaultColDef: {
-        flex: 1,
-      },
-      detailRowHeight: 160,
-      detailCellRendererParams: {
-        detailGridOptions: {
-          columnDefs: [
-            { field: 'userId', headerName: 'User Id' },
-            { field: 'user', headerName: 'User' },
-            { field: 'totalCommits', headerName: 'Total Commits' },
-            { field: 'totalPRs', headerName: 'Total Pull Requests.' },
-            { field: 'totalIssues', headerName: 'Total Issues' },
-          ],
-          defaultColDef: {
-            flex: 1,
-          },
-          innerHeight: '30px'
-        },
-        getDetailRowData: (params: any) => {
-          if (!params.data.isIncluded) {
-            params.successCallback([]);
-            return;
-          }
-
-          params.successCallback([
-            {
-              ...params.data.userRepoDetails[0],
-              user: params.data.username,
-            },
-          ]);
-        },
-      },
+      paginationPageSize: this.pageSize,
+      ...COMMON_GRID_OPTIONS,
     };
   }
   ngOnInit(): void {
-    this.context =  {
-      parentComponent: this
-    }
+    this.context = {
+      parentComponent: this,
+    };
   }
 
   ngOnDestroy(): void {
@@ -115,7 +105,7 @@ export class RepositoriesComponent implements OnInit {
   }
 
   onCheckboxChange(isIncluded: boolean, row: { _id: string }) {
-    console.log('testing')
+    console.log('testing');
     this.githubService.initializeSyncing({ id: row._id, isIncluded }).subscribe(
       (data) => {
         console.log('Success:: ', data);
@@ -127,39 +117,79 @@ export class RepositoriesComponent implements OnInit {
         console.log('Error:: ', error);
         // this._snackBar.open('Something went wrong. Please try again in few minutes.');
       }
-    )
+    );
   }
 
   onGridReady(params: GridReadyEvent) {
     const datasource = this.getServerSideDatasource();
-    params.api.updateGridOptions({ serverSideDatasource: datasource })
+    params.api.updateGridOptions({ serverSideDatasource: datasource });
+  }
+
+  onDetailGridReady(params: GridReadyEvent) {
+    const datasource = this.getServerSideDatasourceForDetails();
+    params.api.updateGridOptions({ serverSideDatasource: datasource });
+  }
+
+  getServerSideDatasourceForDetails(): IServerSideDatasource {
+    return {
+      getRows: (params) => {
+        const startIndex =
+          Math.ceil((params?.request?.startRow || 0) / this.pageSize) + 1;
+
+        this.githubService
+          .getRepositoryDetails(startIndex, this.pageSize)
+          .subscribe(
+            (response) => {
+              const { docs = [], totalDocs = 0 } = response;
+              if (!docs.length && !totalDocs) {
+                params.api.showNoRowsOverlay();
+                params.success({ rowData: [], rowCount: 0 });
+                return;
+              }
+
+              params.api.hideOverlay();
+
+              this.rowData = docs;
+
+              params.success({ rowData: this.rowData, rowCount: totalDocs });
+            },
+            () => {
+              params.fail();
+            }
+          );
+      },
+    };
   }
 
   getServerSideDatasource(): IServerSideDatasource {
     return {
       getRows: (params) => {
-        const startIndex = Math.ceil((params?.request?.startRow || 0) / this.pageSize) + 1
-        
-        this.githubService.getRepositories(startIndex, this.pageSize)
-          .subscribe(response => {
+        const startIndex =
+          Math.ceil((params?.request?.startRow || 0) / this.pageSize) + 1;
+
+        this.githubService.getRepositories(startIndex, this.pageSize).subscribe(
+          (response) => {
             const { docs = [], totalDocs = 0 } = response;
             if (!docs.length && !totalDocs) {
               params.api.showNoRowsOverlay();
-              params.success({ rowData: [], rowCount: 0});
+              params.success({ rowData: [], rowCount: 0 });
               return;
             }
 
             params.api.hideOverlay();
 
-            this.rowData = docs;
+            this.detailRowData = docs;
 
-            params.success({ rowData: this.rowData, rowCount: totalDocs });
+            params.success({
+              rowData: this.detailRowData,
+              rowCount: totalDocs,
+            });
           },
           () => {
-            params.fail()
-          });
+            params.fail();
+          }
+        );
       },
     };
   }
-
 }
